be93bd1e35239bf90747415ede3136c7aef75fbd
rlojiya20798 <rlojiya20798@gmail.com>
2026-09-21 21:41:21 +0900

    submission

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-    char c;
+    int a, b;
 
-    printf("enter a character : ");
-    scanf("%c", &c);
+    printf("input a number : ");
+    scanf("%i", &a);
 
-    printf("the next character of %c (%i) is %c (%i)\n", c, c, c + 1, c + 1);
+    printf("input a number : ");
+    scanf("%i", &b);
+
+    printf("Dividing result is %f\n", (float) a / b);
 
     return 0;
 }
